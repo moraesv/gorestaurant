a55f58718465eede1e6df35fc3c8aa0c4488c0ab
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-03-29 21:20:43 -0300

    Refactor app to typescript and functional components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,24 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IconProps {
+  size: number;
+}
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: (props: IconProps) => JSX.Element;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
